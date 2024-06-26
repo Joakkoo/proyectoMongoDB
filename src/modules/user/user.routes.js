d1d774cbd350d83bfa31e16eb4c13d13fc96bb8c
Joakkoo <joako2503@outlook.com>
2024-06-26 16:52:46 -0300

    Tasks

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -7,11 +7,18 @@ const router = express.Router();
 router.get("/api/user", async (req, res) => {
   // #swagger.tags = ['Usuario']
   try {
+    let params = {}
+    if(req.headers['params'])
+      try{
+        params = JSON.parse(req.headers['params'])
+      }catch(err){
+        console.log('Error parsing params:', error)
+        return res.status(400).send('invalid params');
+      }      
     params = JSON.parse(req.headers['params'])
 
     let paginated = await userService.paginated(params)
     return res.status(200).send(paginated);
-
   } catch (error) {
     console.log(error)
     return res.status(500).send(error);
